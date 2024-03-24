009146bed5f540a7b090d08b0618c64d8964b230
Alvin Alleyne <64805659+AlvinsProjects@users.noreply.github.com>
2024-03-24 13:29:12 -0500

    Initial Commit

---
+++ Sudoku/SudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuApp.swift
+//  Sudoku
+//
+//  Created by Alvin Alleyne on 3/24/24.
+//
+
+import SwiftUI
+
+@main
+struct SudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
