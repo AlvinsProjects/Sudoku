97819c052c7000d3ef978961c59e0d5adfb4b9d9
Alvin Alleyne <64805659+AlvinsProjects@users.noreply.github.com>
2024-03-25 08:08:48 -0500

    First commit

---
+++ Sudoku/Board.swift
@@ -0,0 +1,68 @@
+//
+//  Board.swift
+//  Sudoku
+//
+//  Created by Alvin Alleyne on 3/24/24.
+//
+
+import Foundation
+
+struct Board: Equatable {
+    
+    enum Difficulty: Int, CaseIterable {
+        #if DEBUG
+        case testing = 2
+        #endif
+        case trivial = 10
+        case easy = 20
+        case medium = 24
+        case hard = 27
+        case extreme = 29
+    }
+    
+    
+    let size = 9
+    let difficulty: Difficulty
+    var fullBoard = [[Int]]()
+    var playerBoard = [[Int]]()
+    
+    init(difficulty: Difficulty = .easy) {
+        self.difficulty = difficulty
+        
+        create()
+        prepareForPlay()
+    }
+    
+    mutating private func create() {
+        let numbers = Array(1...size)
+        let positions = [0, 3, 6, 1, 4, 7, 2, 5, 8]
+        
+        let rows = Array(0..<size)
+        let columns = Array(0..<size)
+        
+        for row in rows {
+            var newRow = [Int]()
+            
+            for column in columns {
+                let position = (positions[row] + column) % size
+                newRow.append(numbers[position])
+            }
+            fullBoard.append(newRow)
+            print(newRow)
+        }
+        playerBoard = fullBoard
+    }
+    
+    private mutating func prepareForPlay() {
+        let empties = difficulty.rawValue
+        let allCells = 0..<size * size / 2
+        
+        for cell in allCells.shuffled().prefix(upTo: empties) {
+            let row = cell / size
+            let column = cell % size
+            playerBoard[row][column] = 0
+            playerBoard[8 - row][8 - column] = 0
+        }
+        
+    }
+}
---
+++ Sudoku/CellView.swift
@@ -0,0 +1,72 @@
+//
+//  CellView.swift
+//  Sudoku
+//
+//  Created by Alvin Alleyne on 3/24/24.
+//
+
+import SwiftUI
+
+
+struct CellView: View {
+    
+    enum HighlightState {
+        case standard, highlighted, selected
+        
+        var color: Color {
+            switch self {
+                case .standard:
+                    return .squareStandard
+                case .highlighted:
+                    return .squareHighlighted
+                case .selected:
+                    return .squareSelected
+            }
+        }
+    }
+    
+    let number: Int
+    let selectedNumber: Int
+    let highlightState: HighlightState
+    let isCorrect: Bool
+    var onSelected: () -> Void
+    
+    var displayNumber: String {
+        if number == 0 {
+            return ""
+        } else {
+            return String(number)
+        }
+    }
+    
+    var foregroundColor: Color {
+        if isCorrect {
+            if number == selectedNumber {
+                return .squareTextSame
+            } else {
+                return .squareTextCorrect
+            }
+        } else {
+            return .squareTextWrong
+        }
+    }
+    
+    
+    var body: some View {
+        Button(action: onSelected) {
+            Text(displayNumber)
+                .font(.title)
+                .foregroundStyle(foregroundColor)
+                .frame(maxWidth: 100, maxHeight: 100)
+                .aspectRatio(1, contentMode: .fit)
+                .background(highlightState.color)
+        }
+        .buttonStyle(.plain)
+    }
+}
+
+
+
+#Preview {
+    CellView(number: 3, selectedNumber: 3, highlightState: .standard, isCorrect: true, onSelected: { })
+}
